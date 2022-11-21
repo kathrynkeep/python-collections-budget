701fe5a2f6378e1a70d073c302618cfba84557b2
kathrynkeep <95603685+kathrynkeep@users.noreply.github.com>
2022-11-20 20:26:10 -0500

    module 1

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -12,3 +12,12 @@ for expense in expenses.list
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
+
+most_common()
+top5 = most_common(spending_counter(5))
+
+ax.bar(categories, count)
+
+ax.set_title('# of Purchases by Category')
+
+plt.show()
